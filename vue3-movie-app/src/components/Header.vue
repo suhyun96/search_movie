d7c1e4df533ca5b52099de78ab598366e57a61d3
suhyun96 <jhk8500@gmail.com>
2023-11-10 23:33:11 +0900

    Header - Logo , google fonts
    - google fonts 적용
    - Logo 컴포넌트 작성
    - 스타일 적용

--- vue3-movie-app/src/components/Header.vue
+++ vue3-movie-app/src/components/Header.vue
@@ -1,5 +1,6 @@
 <template lang="">
     <header>
+      <Logo/>
       <!-- nav-pills클래스 사용해서 active클래스 있으면 그거 스타일 적용 -->
       <div class="nav nav-pills">
         <div class="nav-item" v-for="nav in navigations" :key="nav.name">
@@ -12,7 +13,10 @@
       </div>
     </header>
 </template>
+
 <script>
+import Logo from './Logo.vue';
+
 export default {
   data() {
     return {
@@ -31,9 +35,24 @@ export default {
         }
       ]
     }
+  },
+  components: {
+    Logo,
   }
 }
 </script>
-<style lang="">
-    
+<style lang="scss" scoped>
+header {
+  height: 70px;
+  padding: 0 40px;
+  // 수평정렬
+  display: flex;
+  // 아이템들 가운데 정렬
+  align-items: center;
+
+  // header 클래스 안에 있는 Logo클래스만 적용 
+  .logo {
+    margin-right: 40px;
+  }
+}
 </style>
---
+++ vue3-movie-app/src/components/Logo.vue
@@ -0,0 +1,32 @@
+<template lang="">
+  <router-link to="/" class="logo">
+    <span>OMDbAPI</span>.COM
+  </router-link>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style lang="scss" scoped>
+//scss에서 정의된 값들을 스타일에 적용 $black
+//@import "../../node_modules/bootstrap/scss/variables"; 여기에 정의된 거를 가져와서 여기서 사용
+@import "../scss/main.scss";
+
+.logo {
+  font-family: "Oswald", "sans-serif";
+  font-size: 20px;
+  color: $black;
+  text-decoration: none;
+
+  //가상 명시자
+  &:hover {
+    color: $black;
+  }
+
+  span {
+    color: $primary;
+  }
+}
+</style>
